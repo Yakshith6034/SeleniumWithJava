e065e1828aa1a20a4557ae9c26785b70966a727f
Yakshith6034 <144008793+Yakshith6034@users.noreply.github.com>
2024-06-12 10:57:57 +0530

    Update TestRunner.java

--- src/test/java/StepRunner/TestRunner.java
+++ src/test/java/StepRunner/TestRunner.java
@@ -8,6 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/java/Features" },
+		 
 
 		glue = { "com.stepdefinitions" }, plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" },
 
